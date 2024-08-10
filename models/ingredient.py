caf6eb00eb54dc246b83df72d09cb4a76c6c6208
Xander-AJ <xander.kamau7@gmail.com>
2024-08-10 16:25:23 +0300

    removed ingredient model and integrate it to recipe model

--- models/ingredient.py
+++
@@ -1,8 +0,0 @@
-from app import db
-
-
-class Ingredient(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    recipe_id = db.Column(db.Integer, db.ForeignKey("recipe.id"), nullable=False)
-    name = db.Column(db.String(100), nullable=False)
-    quantity = db.Column(db.String(50))
